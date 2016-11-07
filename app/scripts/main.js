6b6e0bc040442705610671f45c3ea4dd32034e7a
Bogdan <d.bogdan@inbox.ru>
2016-11-08 00:41:32 +0700

    add 2 pages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,5 +9,52 @@ $(document).ready(function () {
         $('.mobile-menu').toggleClass('active');
         $('.page-wrapper').toggleClass('active');
     });
-    
+
+
+    // Дропдаун меню в каталоге
+
+    $('.catalog-nav-list > li > a').click(function(e) {
+
+        if ($(this).next('ul').length) {
+
+            e.preventDefault();
+
+            $(this).toggleClass('active');
+
+            $(this).next().slideToggle();
+        }
+
+    });
+
+    // триггер каталога на мобильных
+
+    $('.js-aside-nav-trigger').click(function(e) {
+
+        e.preventDefault();
+
+        $(this).toggleClass('active');
+        $('.catalog-nav').slideToggle();
+
+    });
+
+    $('.popular-items-slider .wrapper').slick({
+        variableWidth: true,
+        slidesToShow: 4,
+        infinite: true,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    centerMode: true
+                }
+            }
+        ]
+    });
+
+    $('.reviews-slider .wrapper').slick({
+        autoplay: true,
+        prevArrow: $('.reviews-slider .arrow-left'),
+        nextArrow: $('.reviews-slider .arrow-right')
+    });
 });

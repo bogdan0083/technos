3069ede926bad37c0588df87a4d97dc7bb7ce4c0
Bogdan <d.bogdan@inbox.ru>
2016-11-10 22:54:18 +0700

    add few pages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,6 @@
-
 $(document).ready(function () {
 
-    $('.nav-trigger-wrap').on('click', function(e) {
+    $('.nav-trigger-wrap').on('click', function (e) {
         e.preventDefault();
 
         $(this).find('.nav-trigger').toggleClass('active');
@@ -13,7 +12,7 @@ $(document).ready(function () {
 
     // Дропдаун меню в каталоге
 
-    $('.catalog-nav-list > li > a').click(function(e) {
+    $('.catalog-nav-list > li > a').click(function (e) {
 
         if ($(this).next('ul').length) {
 
@@ -28,7 +27,7 @@ $(document).ready(function () {
 
     // триггер каталога на мобильных
 
-    $('.js-aside-nav-trigger').click(function(e) {
+    $('.js-aside-nav-trigger').click(function (e) {
 
         e.preventDefault();
 
@@ -57,4 +56,122 @@ $(document).ready(function () {
         prevArrow: $('.reviews-slider .arrow-left'),
         nextArrow: $('.reviews-slider .arrow-right')
     });
+
+    // слайдер товара
+
+    $('.full-item-preview-slider').slick({
+        asNavFor: '.thumbnails-slider .wrapper',
+        dots: false,
+        arrows: false,
+        infinite: false,
+    });
+
+
+    $('.full-item-preview-slider').on('beforeChange', function(e, slick, cur, next) {
+
+        $('.thumbnails-slider')
+            .find('.slide')
+            .removeClass('slick-current')
+            .eq(next)
+            .addClass('slick-current');
+
+    });
+
+
+
+    // миниатюры в товаре.
+    $('.thumbnails-slider .wrapper').slick({
+        slidesToShow: 3,
+        initialSlide: 2,
+        asNavFor: '.full-item-preview-slider',
+        focusOnSelect: true,
+        variableWidth: true,
+        mobileFirst: true,
+        centerMode: true,
+        infinite: false,
+        prevArrow: $('.thumbnails-slider .arrow-left'),
+        nextArrow: $('.thumbnails-slider .arrow-right'),
+        responsive: [{
+            breakpoint: 880,
+            settings: {
+                slidesToShow: 4,
+                variableWidth: false,
+                vertical: true,
+                initialSlide: 0,
+                centerMode: false,
+                verticalSwiping: true
+            }
+        }]
+    });
+
+
+    // слайдер новостей (на мобильном)
+
+    $('.news-section-slider .row').slick({
+        autoplay: true,
+        variableWidth: true,
+        mobileFirst: true,
+        centerMode: true,
+        prevArrow: $('.news-section-slider .square-arrow-left'),
+        nextArrow: $('.news-section-slider .square-arrow-right'),
+        responsive: [
+            {
+                breakpoint: 880,
+                settings: 'unslick'
+            }
+        ]
+    });
+
+    $('.fancybox-modal').fancybox({
+        closeBtn: false,
+        fitToView: false,
+        scrolling: 'visible',
+        padding: 0
+    });
+
+    $('.fancybox').fancybox();
+    
+    initTabs();
+
+    // Простая реализация табов
+
+    // pane-tab - это наш триггер табов.
+    // tab-content - содержание таба
+
+    function initTabs() {
+
+        $('.tab-pane').click(onPaneClick);
+
+    }
+
+    function onPaneClick(e) {
+        e.preventDefault();
+
+        // таргет по которому кликнули
+        var $clickedPane = $(this),
+
+        // все триггеры табов
+            tabgroup = $clickedPane.data('grouptrigger'),
+            $panes = $('[data-grouptrigger="' + tabgroup + '"]'),
+
+        // содержимое всех табов
+            $tabContents = $('[data-tabgroup="' + tabgroup + '"]'),
+
+
+
+            href = $clickedPane.data('href'),
+
+        // нужное содержимое таба
+            $targetTabContent = $('#' + href);
+
+        console.log($targetTabContent);
+
+        $panes.removeClass('active');
+
+        $clickedPane.addClass('active');
+
+        $tabContents.removeClass('active');
+
+        $targetTabContent.addClass('active');
+    }
 });
